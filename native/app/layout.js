19947268d3bfb3d4d1d0e96a95a33cd75303fb37
root <root@instance-1.europe-central2-c.c.libri-385108.internal>
2023-04-30 18:14:26 +0000

    add bg

--- native/app/layout.js
+++ native/app/layout.js
@@ -1,18 +1,19 @@
-import { Jost } from 'next/font/google'
+import { Jost } from "next/font/google";
+import '../global.css'
 
-const inter = Jost({ subsets: ['latin'] })
+const inter = Jost({ subsets: ["latin"] });
 
 export const metadata = {
-  title: '🚀 Native Language',
-  description: 'Native Language App 🚀',
-}
+  title: "🚀 Native Language",
+  description: "Native Language App 🚀",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} w-1/2 mx-auto h-screen`}>
+      <body className={`${inter.className} p-0 m-0 bg-[#30336] bg-radial-gradient bg-size-50`}>
         {children}
       </body>
     </html>
-  )
+  );
 }
