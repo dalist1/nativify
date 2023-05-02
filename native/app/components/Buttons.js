e232de666c2781a06f3c46ca667ff8a7034b8543
root <root@instance-1.europe-central2-c.c.libri-385108.internal>
2023-05-02 15:00:14 +0000

    add TTS

--- native/app/components/Buttons.js
+++ native/app/components/Buttons.js
@@ -2,16 +2,18 @@
 
 import { useState } from "react";
 
+function playAudio(word) {
+  const audioUrl = `https://gttsfastapi.vercel.app/tts/sq?text=${word}`;
+  const audio = new Audio(audioUrl);
+  audio.autoplay = true;
+}
+
+
 export default function Buttons({ alphabet, words }) {
   const [displayWords, setDisplayWords] = useState([]);
 
   const pickWord = (letter) => {
     const wordsList = words.filter((item) => item.letter === letter);
-    console.log(
-      `you picked ${letter} and there is simple words represents the ${wordsList[0].items.join(
-        " "
-      )}`
-    );
     setDisplayWords(wordsList[0].items);
   };
   return (
@@ -31,8 +33,11 @@ export default function Buttons({ alphabet, words }) {
       <div className="flex pt-10 space-x-5 py-4 text-xl flex-wrap justify-center items-center ">
         {displayWords.length > 0 &&
           displayWords.map((word, key) => (
-            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
-              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 " key={key}>
+            <button key={key} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800" onClick={ () => playAudio(word)}>
+              <span
+                className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 "
+                key={key}
+              >
                 {word}
               </span>
             </button>
